aa8d68ce8995421168bbb0e8f42dccecb69d2c8b
hankliu <397694072@qq.com>
2024-04-07 09:58:31 +0800

    feat(build): remove autobase-webpack-plugin

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 const path = require("node:path");
 const withLess = require("next-with-less");
-const AutoBaseWebpackPlugin = require("autobase-webpack-plugin");
+// const AutoBaseWebpackPlugin = require("autobase-webpack-plugin");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix:
-    process.env.NODE_ENV === "production" && process.env.GITHUB_ACTIONS
-      ? "/resume/"
-      : "",
   output: "export",
   lessLoaderOptions: {
     lessOptions: {
@@ -24,10 +20,13 @@ const nextConfig = {
     unoptimized: true,
   },
   transpilePackages: ["antd"],
-  webpack: (config) => {
-    config.plugins.push(new AutoBaseWebpackPlugin());
-    return config;
+  env: {
+    ROUTE_PREFIX: "",
   },
+  // webpack: (config) => {
+  //   config.plugins.push(new AutoBaseWebpackPlugin());
+  //   return config;
+  // },
   // async headers() {
   //   return [
   //     {
@@ -42,5 +41,21 @@ const nextConfig = {
   //   ];
   // },
 };
+
+// 是否通过github actions部署
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+  // 用于为静态资源（如图像、样式表、JavaScript 文件等）设置 URL 前缀
+  // 这在将应用部署到自定义域名或 CDN 上时特别有用，因为它允许您将静态资源存储在不同的位置
+  nextConfig.assetPrefix = `/${repo}/`;
+  // 用于为应用设置基础路径
+  // 这在将应用部署到子目录下时特别有用，因为它允许您指定应用所在的目录
+  nextConfig.basePath = `/${repo}`;
+  nextConfig.env.ROUTE_PREFIX = `/${repo}`;
+
+  console.log("next config:", nextConfig);
+}
 
 module.exports = withLess(nextConfig);
